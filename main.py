181fe7e26046175242c6d82de9254eb2a15d4f93
ion05 <aayanagarwal05@gmail.com>
2022-08-25 16:24:12 +0530

    fixes

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+import imp
+from flask import Flask, request
+import os.path
+from requests.sessions import session
+from flask_cors import CORS, cross_origin
 from scipy.optimize import linprog
-obj = ['-25', '-27', '-27', '980']
-lhs_ineq = [
-    [1, 1, 1, 0],
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [-1, -1, -1, 0]
-]
-rhs_ineq = [
-    20,
-    10,
-    20,
-    15,
-    -5
-]
-opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,)
-print(opt)
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@cross_origin()
+def algo():
+
+    obj = ['-25', '-27', '-27', '980']
+    lhs_ineq = [
+        [1, 1, 1, 0],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [-1, -1, -1, 0]
+    ]
+
+    rhs_ineq = [
+        20,
+        10,
+        20,
+        15,
+        -5
+    ]
+    opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,)
+    print(opt.x)
+    print(type(opt.x))
+    return(opt.x.tolist())
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
